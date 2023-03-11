348cb0c3fc5fabd0bc7335ffaa3b12177a37e358
Ali Pourgheysari <a.geysar@gmail.com>
2023-03-11 23:07:56 +0330

    add more classes to store model
    these classes are: Address, Collection, Cart, CartItem, and OrderItem

--- store/models.py
+++ store/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models.Model):
+    title = models.CharField(100)
+
 class Product(models.Model):
-    Title = models.CharField(max_length=100)
-    Description = models.TextField()
-    Price = models.DecimalField(max_digits=6, decimal_places=2)
-    Inventory = models.IntegerField()
-    Last_Update = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 class Customer(models.Model):
     MEMBERSHIP_GOLD = 'G'
@@ -17,12 +22,12 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_BRONZE, 'Bronze'),
     ]
-    First_Name = models.CharField(max_length=50)
-    Last_Name = models.CharField(max_length=50)
-    Email = models.EmailField(unique=True)
-    Phone = models.CharField(max_length=20)
-    Birth_Date = models.DateField(null=True)
-    Membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
 class Order(models.Model):
     PAYMENT_STATUS_COMPLETE = 'C'
@@ -33,5 +38,25 @@ class Order(models.Model):
         (PAYMENT_STATUS_PENDING, 'Pending'),
         (PAYMENT_STATUS_FAILED, 'Failed'),
     ]
-    Placed_At = models.DateTimeField(auto_now_add=True)
-    Payment_Status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    placed_At = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OredrItem(models.Model):
+    quantity = models.PositiveBigIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.PROTECT) 
+    product = models.ForeignKey(Product, on_delete=models.PROTECT) 
+
+class Address(models.Model):
+    city = models.CharField(max_length=255)
+    street = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    quantity = models.PositiveBigIntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE) 
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
