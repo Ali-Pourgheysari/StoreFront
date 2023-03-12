5c030acffdc66f99ea27afb9a57ea60a0045ce5d
Ali Pourgheysari <a.geysar@gmail.com>
2023-03-12 21:03:38 +0330

    add classes to tag model
    including some changes in the store's model

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length=255)
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # GenericType for product
+    contect_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    content_id = models.PositiveBigIntegerField()
+    content_object =  GenericForeignKey()
