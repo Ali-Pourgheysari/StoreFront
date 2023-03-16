a5d6f388f0a77dcb5f88938d6c6c765b1cadb690
Ali Pourgheysari <a.geysar@gmail.com>
2023-03-16 16:38:07 +0330

    register models in admin area

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 from . import models
+
 
 # Register your models here.
 @admin.register(models.Product)
@@ -22,4 +26,27 @@ class customerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
 
-admin.site.register(models.Collection)
+@admin.register(models.Order)
+class orderAdmin(admin.ModelAdmin):
+    list_display = ['customer_name','payment_status']
+    list_per_page = 10
+    list_select_related = ['customer']
+
+    @admin.display(ordering='customer')
+    def customer_name(self, order):
+        return order.customer.first_name + ' ' + order.customer.last_name
+
+@admin.register(models.Collection)
+class collectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+    list_per_page = 10
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = reverse('admin:store_product_changelist') + '?' + urlencode({'collection__id': collection.id})
+        return format_html(f'<a href="{url}">{collection.products_count}</a>')
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count = Count('product')
+        )
